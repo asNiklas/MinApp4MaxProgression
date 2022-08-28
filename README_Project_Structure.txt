a54eb4872c91434b8a26c702c369bc1834ce3428
asNiklas <niklas.the.assfalg@gmail.com>
2022-08-28 15:30:36 +0200

    first commit

---
+++ README_Project_Structure.txt
@@ -0,0 +1,1 @@
+https://bodo-schoenfeld.de/projektstruktur-python/
---
+++ src/MinApp4MaxProgression/main.py
@@ -0,0 +1,6 @@
+def ausgeben(text: str) -> None:
+    print(f"Text: {text}")
+
+
+if __name__ == "__main__":
+    ausgeben("What is what?")
